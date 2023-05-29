5e3f0f64671bae3a0eecc4214b40b26c90b34132
HannahMidd <hmiddleton1017@gmail.com>
2023-05-29 17:49:18 -0400

    adding questions for quiz

--- assets/questions.js
+++ assets/questions.js
@@ -1,6 +1,18 @@
 // list of all questions, choices, and answers
 var questions = [
   {
+    title:
+      "What is the term for the technique that promotes self-reflection and problem-solving skills in debugging?",
+    choices: [
+      "Rubber Duck Debugging",
+      "Deep Breath & Debug",
+      "Debugger Slugger",
+      "Dont miss it, fix it",
+    ],
+    answer: "Rubber Duck Debugging",
+  },
+
+  {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts",
@@ -10,6 +22,7 @@ var questions = [
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses",
   },
+
   {
     title: "Arrays in JavaScript can be used to store ____.",
     choices: [
@@ -20,6 +33,30 @@ var questions = [
     ],
     answer: "all of the above",
   },
+
+  {
+    title: "When writing a conditional statement, what order should we follow?",
+    choices: [
+      "Else if, Else, if",
+      "If, Else if, Else",
+      "Else if, If, Else",
+      "Else if, Else, if",
+    ],
+    answer: "If, Else if, Else",
+  },
+
+  {
+    title:
+      "In the following for loop example, i++ signifies what? for (var i=0; i < i < names.length, i++)",
+    choices: [
+      "Where to stop the loop",
+      "The iteration step, by 1 each step",
+      "Where to begin the loop",
+      "The iteration step, by 2 each step",
+    ],
+    answer: "The iteration step, by 1 each step",
+  },
+
   {
     title:
       "String values must be enclosed within ____ when being assigned to variables.",
