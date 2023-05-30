76e4762c403237f817ec786688d6d50a63a99a08
HannahMidd <hmiddleton1017@gmail.com>
2023-05-29 20:03:15 -0400

    adding additional info to JS, CSS and HTML

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,6 +1,6 @@
 // variables created via elements in the Document Object Model (DOM)
-var startButton = document.getElementById('start-button')
-var TimerEl = document.getElementById("timer-text")
+var startButton = document.getElementById("start-button");
+var TimerEl = document.getElementById("timer-text");
 var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
@@ -9,13 +9,23 @@ var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
 
 // Timer count that gives testers 90 seconds to answer all questions
-let count = 90; 
+let count = 90;
 
 // Function to begin countdown
 startButton.addEventListener("click", function () {
-setInterval(function (){
-count -= 1;
-TimerEl.textContent= count;
-}, 1000);
-
+  setInterval(function () {
+    count -= 1;
+    TimerEl.textContent = count;
+  }, 1000);
 });
+
+// Hides previous quiz messages
+function beginQuiz() {
+  var beginQuizEl = document.getElementById("start-screen");
+  beginQuizEl.setAttribute("class", "hide");
+}
+
+// Unhides questions
+questionsEl.removeAttribute("class");
+
+// left off around line 33
