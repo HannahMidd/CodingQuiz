8ea726da18eda9f3a162855de37a71bb20e8753b
HannahMidd <hmiddleton1017@gmail.com>
2023-05-30 12:02:53 -0400

    adding style to questions

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -17,27 +17,77 @@ startButton.addEventListener("click", function () {
     count -= 1;
     TimerEl.textContent = count;
   }, 1000);
+  
 });
 
 // Hides previous quiz messages
 function beginQuiz() {
   var beginQuizEl = document.getElementById("start-screen");
-  beginQuizEl.setAttribute("class", "hide");
+//   beginQuizEl.setAttribute("class", "hide");
+//   Event listener to begin quiz
+  startButton.addEventListener("click", beginQuiz);
+  // Unhides questions
+  questionsEl.removeAttribute("class");
+
+  getQuestion();
 }
-
-// Unhides questions
-questionsEl.removeAttribute("class");
-
-getQuestion();
 
 // Function to grab question from array
 function getQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
+
+  // Update the page title and options with current question and answers
+  var questionTitleEl = document.getElementById("question-title");
+  questionTitleEl.textContent = currentQuestion.title;
+
+  // Reset choice string empty
+  choicesEl.innerHTML = "";
+
+  //   For loop to go through questions 1 by 1
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    // creating buttons
+    var choice = currentQuestion.choices[i];
+    var choiceButton = document.createElement("button");
+    choiceButton.setAttribute("class", "choice");
+    choiceButton.setAttribute("value", choice);
+    choiceButton.textContent = i + 1 + ". " + choice;
+
+    // To show on page, we need to append the answer options
+    choicesEl.appendChild(choiceButton);
+  }
 }
 
-// Update the page title and options with current question and answers
-var questionTitleEl = document.getElementById("question-title");
-questionTitleEl.textContent = currentQuestion.title;
+function questionClick(event) {
+  var buttonEl = event.target;
 
-// Reset choice string empty
-  choicesEl.innerHTML = '';
+  //   If question was answered wrong
+  if (buttonEl.value !== questions[currentQuestionIndex].answer) {
+    // Minus 15 seconds from clock
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+
+    feedbackEl.textContent = "Wrong!";
+  } else {
+    feedbackEl.textContent = "Correct!";
+  }
+
+  // flash right/wrong feedback on page for 1 second
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function () {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  //   Grab next question unless we are out of questions
+  currentQuestionIndex++;
+  if (time <= 0 || currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+// Check out line 110
+startButton.onclick = beginQuiz;
