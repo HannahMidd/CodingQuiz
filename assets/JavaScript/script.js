3f32f78a5e77f5e60b05a450298ab14bfe643e87
HannahMidd <hmiddleton1017@gmail.com>
2023-05-31 21:45:22 -0400

    adding final debugging fixes to line 81

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -76,16 +76,22 @@ function hideFeedback() {
 }
 function questionClick(event) {
   var buttonEl = event.target;
+  console.log("eventfeedbackline81");
 
+  
   // If question was answered wrong
   if (buttonEl.value !== questions[currentQuestionIndex].answer) {
     // Minus 15 seconds from clock
     time -= 15;
-
+    
     if (time < 0) {
       time = 0;
     }
-
+    
+    // User clicks but not on a choice
+    if (!buttonEl.matches(".choice")) {
+      return;
+    }
     // Reflect time loss on page
     feedbackEl.style.display = "block";
     timerEl.textContent = time;
@@ -165,37 +171,6 @@ function checkForEnter(event) {
     saveHighscore();
   }
 }
-
-// --------------------------------Show Highscores Section-----------------------------
-function showHighscores() {
-  // grab scores from localstorage or begin empty array
-  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-  // Shows Highscores, best first
-  highscores.sort(function (a, b) {
-    return b.score - a.score;
-  });
-
-  for (var i = 0; i < highscores.length; i += 1) {
-    // create li tag for each high score
-    var liTag = document.createElement("li");
-    liTag.textContent = highscores[i].initials + " - " + highscores[i].score;
-
-    // display on page
-    var olEl = document.getElementById("highscores");
-    olEl.appendChild(liTag);
-  }
-}
-
-function clearHighscores() {
-  window.localStorage.removeItem("highscores");
-  window.location.reload();
-}
-
-document.getElementById("clear").onclick = clearHighscores;
-
-// call the function
-showHighscores();
 
 // ------------------------------------ Clicks -----------------------------------------------
 startBtn.onclick = beginQuiz;
